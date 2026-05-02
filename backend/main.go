98c9199b00e8a03f2cdbcd03b0414d5c2a125873
Liang Zhuang <bzhuangliang@gmail.com>
2026-05-02 10:27:58 -0700

    update entry function

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"nft-auction-backend/api"
+	"nft-auction-backend/config"
+	"nft-auction-backend/repo"
+)
 
 func main() {
-	fmt.Println("Hello, NFT Auction Go Backend!")
+	log.Println("Starting app...")
+
+	db := config.ConnectDB()
+
+	listingRepo := repo.NewListingRepo(db)
+	auctionRepo := repo.NewAuctionRepo(db)
+	bidRepo := repo.NewBidRepo(db)
+
+	log.Println("Database connected")
+
+	api.StartServer(listingRepo, auctionRepo, bidRepo)
 }
