a0f94d7e281fd651ad77089aa77e1d87cca2469b
Liang Zhuang <bzhuangliang@gmail.com>
2026-05-03 22:23:56 -0700

    add auction id field to Auction model

--- backend/models/auction.go
+++ backend/models/auction.go
@@ -1,7 +1,8 @@
 package models
 
 type Auction struct {
-	ID          uint64 `gorm:"primaryKey"`
+	ID          uint64 `gorm:"primaryKey"`  // DB internal ID
+	AuctionID   string `gorm:"uniqueIndex"` // on-chain ID
 	Seller      string
 	NftContract string
 	TokenId     string
